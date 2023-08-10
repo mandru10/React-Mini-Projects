cca44c631e041bf3705ef3811c98b60c031a53df
Catalin Mandru <112318980+mandru10@users.noreply.github.com>
2023-08-10 14:20:37 +0300

    Update Course.js

--- Buy Movies/Course.js
+++ Buy Movies/Course.js
@@ -2,27 +2,26 @@ import React, {Component} from "react";
 
 class Course extends Component{
 
+    clicker(){
+        let active = !this.state.active;
+        this.setState({active: active});
+        this.props.sumPrice(active ? this.props.price : -this.props.price);
+
+    }
+    
     constructor(props){
         super(props);
 
-        this.state = {
-            active : false
+        this.state={
+            active: false
         }
-
-        this.click = this.clock.bind(this)
-
+        this.clicker = this.clicker.bind(this);
     }
-
-    click(){
-        let active = !this.state.active
-        this.setState({active : active});
-        this.props.calculate(active ? this.props.price : -this.props.price);
-    }
-
+    
     render(){
         return(
             <div>
-                <p className={this.state.active ? 'active' : ''} onClick={this.click}>{this.props.title} <b>{this.props.price}</b></p>
+                <p className={this.state.active ? 'active' : ''} onClick={this.clicker}>{this.props.name} <b>{this.props.price}</b></p>
             </div>
         )
     }
