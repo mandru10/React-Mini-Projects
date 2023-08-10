9d92c96c4a79c9f77932df0337d1c4f3a8868400
Catalin Mandru <112318980+mandru10@users.noreply.github.com>
2023-08-10 14:19:07 +0300

    Update App.js

--- Buy Movies/App.js
+++ Buy Movies/App.js
@@ -6,19 +6,19 @@ import Coursesales from './Coursesales';
 class App extends Component {
   render(){
     let movies = [
-      { name: 'Thor', price: 100 },
-      { name: 'Batman', price: 140 },
-      { name: 'Superman', price: 160 },
-      { name: 'Flash', price: 110 }
-    ];
+      { name: 'Spiderman', price: 100 },
+      { name: 'Batman', price: 200 },
+      { name: 'Flash', price: 300 },
+      { name: 'Thor', price: 400 },
 
+    ]
   return (
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to Course Purchase Page</h2>
       </div>
-      <Coursesales movies={movies}/>
+      <Coursesales items={movies}/> 
     </div>
   );
 }
