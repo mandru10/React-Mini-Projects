23b696a00ea2411641cebc80cab4af9467b327b5
Catalin Mandru <112318980+mandru10@users.noreply.github.com>
2023-08-10 14:20:06 +0300

    Update Coursesales.js

--- Buy Movies/Coursesales.js
+++ Buy Movies/Coursesales.js
@@ -1,34 +1,36 @@
-import React, { Componenet } from "react";
-import Course from './Course';
+import React, { Component } from "react";
+import Course from "./Course";
 
-class Coursesales extends Componenet{
+class Coursesales extends Component{
+    sumPrice(price){
+        this.setState({total: this.state.total + price})
+    }
     
     constructor(props){
-        super(props)
+        super(props);
 
         this.state = {
-            total : 0
-        }
-
-        this.calculate = this.calculate.bind(this)
-
+            total: 0
+        };
+        this.sumPrice = this.sumPrice.bind(this);
     }
 
-     calculate(price){
-            this.setState({ total: this.state.total + price })
-        }
-    
+
     render(){
-        let movies = this.props.movies.map((index, i) => {
-            return <Course title={index.title} price={index.price} key={i} calculate={this.calculate} active={index.active}/>
+        let movies = this.props.items.map((item, i) => {
+            return <Course name={item.name} price={item.price}
+            key={i} sumPrice={this.sumPrice} active={item.active} />
         });
         return(
-            <div id='courses'>
-                {movies}
-                <p id='total'>Total {this.state.total}</p>
+            <div>
+                <h1>You can buy movies :</h1>
+                <div id='courses'>
+                    {movies}
+                    <p id='total'> Total <b>{this.state.total}</b></p>
+                </div>
             </div>
         )
     }
 }
 
-export default Coursesales;
+export default Coursesales
